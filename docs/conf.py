e0dfbbaaa48534bcb2a574a31d5878f68fa1c217
Frank Nightingale <102726304+fnight128@users.noreply.github.com>
2023-07-20 12:24:20 +0100

    Create docs/conf.py

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+todo_include_todos = True
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+exclude_patterns = []
+add_function_parentheses = True
+#add_module_names = True
+# A list of ignored prefixes for module index sorting.
+#modindex_common_prefix = []
+
+project = u'MagpiEM'
+
+version = ''
+release = ''
+
+# -- Options for HTML output ---------------------------------------------------
+
+html_theme = 'renku'
+html_theme_path = ['themes']
+html_title = "MagpiEM"
+#html_short_title = None
+#html_logo = None
+#html_favicon = None
+html_static_path = ['_static']
+html_domain_indices = False
+html_use_index = False
+html_show_sphinx = False
+htmlhelp_basename = 'MagpiEMdoc'
+html_show_sourcelink = False
