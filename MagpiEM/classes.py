bbed39449eca95d49e903b59ff0bb622469eb362
Frank Nightingale <fnight128@gmail.com>
2025-08-19 15:19:41 +0100

    Black

--- MagpiEM/classes.py
+++ MagpiEM/classes.py
@@ -783,7 +783,10 @@ class Tomogram:
         # Necessary for reproducible lattice assignment and comparison with C++
         sorted_particles = sorted(self.all_particles, key=lambda p: p.particle_id)
         for particle in sorted_particles:
-            if particle.lattice or len(particle.neighbours) < self.cleaning_params.min_neighbours:
+            if (
+                particle.lattice
+                or len(particle.neighbours) < self.cleaning_params.min_neighbours
+            ):
                 continue
             particle.choose_new_lattice(len(self.lattices))
 
